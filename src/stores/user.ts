cdda543fc9a3551db68348f0c3a1d63095ccf12d
Yurii Rybachok <drummer2009@ukr.net>
2024-04-22 10:24:55 +0300

    create routing for pages

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -2,7 +2,7 @@ import { ref } from 'vue'
 import { defineStore } from 'pinia'
 
 export const useUserStore = defineStore('user', () => {
-  const userId = ref<string>('')
+  const userId = ref<string>('stephan')
 
   return { userId }
 })
