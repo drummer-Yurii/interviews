33b34d28ea6f0eab92de8113cce34d5d187a8374
Yurii Rybachok <drummer2009@ukr.net>
2024-04-22 09:51:43 +0300

    create user store & sign out icon

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import { useUserStore } from '@/stores/user'
+
+const userStore = useUserStore()
 
 interface IMenuItem {
   label: string
@@ -35,9 +38,15 @@ const items = ref<IMenuItem[]>([
   <app-menubar :model="items" class="menu">
     <template #item="{ item, props }">
       <router-link :to="item.path" class="flex align-items-center" v-bind="props.action">
-        <span :class="item.icon" class="p-menuitem-icon"></span>
+        <span :class="item.icon" class="p-menuitem-icon" />
         <span class="ml-2">{{ item.label }}</span>
       </router-link>
+    </template>
+    <template #end>
+      <span class="flex align-items-center menu-exit">
+        <span class="pi pi-sign-out p-menuitem-icon" />
+        <span class="ml-2">Sign Out</span>
+      </span>
     </template>
   </app-menubar>
 </template>
