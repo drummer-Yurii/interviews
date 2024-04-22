0f21a146352c6925ee83b827f709718edc7e4e1e
Yurii Rybachok <drummer2009@ukr.net>
2024-04-22 10:02:36 +0300

    show dynamic icons for autorization & sign out

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
+import type { ComputedRef } from 'vue'
 import { useUserStore } from '@/stores/user'
 
 const userStore = useUserStore()
@@ -8,28 +9,33 @@ interface IMenuItem {
   label: string
   icon: string
   path: string
+  show: ComputedRef<boolean>
 }
 
 const items = ref<IMenuItem[]>([
   {
     label: 'Authorization',
     icon: 'pi pi-user',
-    path: '/auth'
+    path: '/auth',
+    show: computed((): boolean => !userStore.userId)
   },
   {
     label: 'Add',
     icon: 'pi pi-plus',
-    path: '/'
+    path: '/',
+    show: computed((): boolean => !!userStore.userId)
   },
   {
     label: 'List of interviews',
     icon: 'pi pi-list',
-    path: '/list'
+    path: '/list',
+    show: computed((): boolean => !!userStore.userId)
   },
   {
     label: 'Statistics',
     icon: 'pi pi-chart-pie',
-    path: '/statistic'
+    path: '/statistic',
+    show: computed((): boolean => !!userStore.userId)
   }
 ])
 </script>
@@ -37,13 +43,19 @@ const items = ref<IMenuItem[]>([
 <template>
   <app-menubar :model="items" class="menu">
     <template #item="{ item, props }">
-      <router-link :to="item.path" class="flex align-items-center" v-bind="props.action">
-        <span :class="item.icon" class="p-menuitem-icon" />
-        <span class="ml-2">{{ item.label }}</span>
-      </router-link>
+      <template v-if="item.show">
+        <router-link :to="item.path" class="flex align-items-center" v-bind="props.action">
+          <span :class="item.icon" class="p-menuitem-icon" />
+          <span class="ml-2">{{ item.label }}</span>
+        </router-link>
+      </template>
     </template>
     <template #end>
-      <span class="flex align-items-center menu-exit">
+      <span
+        v-if="userStore.userId"
+        @click="userStore.userId = ''"
+        class="flex align-items-center menu-exit"
+      >
         <span class="pi pi-sign-out p-menuitem-icon" />
         <span class="ml-2">Sign Out</span>
       </span>
