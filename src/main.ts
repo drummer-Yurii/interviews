50ff8b2e4c572fc129fbb942da1c1b7332329504
Yurii Rybachok <drummer2009@ukr.net>
2024-04-22 10:44:47 +0300

    install firebase & setup

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import 'primevue/resources/themes/saga-blue/theme.css'
 import 'primeicons/primeicons.css'
 import 'primeflex/primeflex.css'
 
+import { initializeApp } from "firebase/app";
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config';
@@ -10,6 +11,17 @@ import Menubar from 'primevue/menubar';
 
 import App from './App.vue'
 import router from './router'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDzsQ3bWmJEhc8oELgWaMBjnJTAyLwEKqw",
+  authDomain: "interviews-b2c7b.firebaseapp.com",
+  projectId: "interviews-b2c7b",
+  storageBucket: "interviews-b2c7b.appspot.com",
+  messagingSenderId: "666880540191",
+  appId: "1:666880540191:web:5247438eafd472d5d9df2a"
+};
+
+initializeApp(firebaseConfig);
 
 const app = createApp(App)
 
