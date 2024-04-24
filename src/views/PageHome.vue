5e2a6203adf8865826a4693e040e4ff4433e7f5f
Yurii Rybachok <drummer2009@ukr.net>
2024-04-24 14:41:22 +0300

    create home page for interview

--- src/views/PageHome.vue
+++ src/views/PageHome.vue
@@ -1,6 +1,63 @@
 <script setup lang="ts">
+import { computed, ref } from 'vue'
+
+const company = ref<string>('')
+const vacancyLink = ref<string>('')
+const hrName = ref<string>('')
+const contactTelegram = ref<string>('')
+const contactWhatsApp = ref<string>('')
+const contactPhone = ref<string>('')
+
+const loading = ref<boolean>(false)
+
+const addNewInterview = () => {}
+
+const disabledSaveButton = computed<boolean>(() => {
+  return !(company.value && vacancyLink.value && hrName.value)
+})
 </script>
 
 <template>
-  Some Content
+  <div class="content-interview">
+    <app-card>
+      <template #title>New Interview</template>
+      <template #content>
+        <app-input-text class="input mb-3" placeholder="Company" v-model="company" />
+        <app-input-text
+          v-model="vacancyLink"
+          class="input mb-3"
+          placeholder="Vacancy Description (Link)"
+        />
+        <app-input-text v-model="hrName" class="input mb-3" placeholder="Contact (name)" />
+        <app-input-text
+          v-model="contactTelegram"
+          class="input mb-3"
+          placeholder="Telegram username HR"
+        />
+        <app-input-text
+          v-model="contactWhatsApp"
+          class="input mb-3"
+          placeholder="WhatsApp phone Nik HR"
+        />
+        <app-input-text v-model="contactPhone" class="input mb-3" placeholder="Phone HR" />
+        <app-button
+          @click="addNewInterview"
+          label="Create Interview"
+          :disabled="disabledSaveButton"
+          :loading="loading"
+        >
+        </app-button>
+      </template>
+    </app-card>
+  </div>
 </template>
+
+<style scoped>
+.input {
+  width: 100%;
+}
+.content-interview {
+  max-width: 600px;
+  margin: auto;
+}
+</style>
