22f758f974af58d8835a4ef716e059ad964babf7
Yurii Rybachok <drummer2009@ukr.net>
2024-04-24 18:24:15 +0300

    create interview & save data to firestore db

--- src/views/PageHome.vue
+++ src/views/PageHome.vue
@@ -1,5 +1,13 @@
 <script setup lang="ts">
 import { computed, ref } from 'vue'
+import { useRouter } from 'vue-router'
+import { getAuth } from 'firebase/auth'
+import { getFirestore, setDoc, doc } from 'firebase/firestore'
+import type { IInterview } from '@/interfaces'
+import { v4 as uuidv4 } from 'uuid'
+
+const db = getFirestore()
+const router = useRouter()
 
 const company = ref<string>('')
 const vacancyLink = ref<string>('')
@@ -10,7 +18,25 @@ const contactPhone = ref<string>('')
 
 const loading = ref<boolean>(false)
 
-const addNewInterview = () => {}
+const addNewInterview = async (): Promise<void> => {
+  loading.value = true
+  const payload: IInterview = {
+    id: uuidv4(),
+    company: company.value,
+    vacancyLink: vacancyLink.value,
+    hrName: hrName.value,
+    contactTelegram: contactTelegram.value,
+    contactWhatsApp: contactWhatsApp.value,
+    contactPhone: contactPhone.value,
+    createdAt: new Date()
+  }
+  const userId = getAuth().currentUser?.uid
+  if (userId) {
+    await setDoc(doc(db, `users/${userId}/interviews`, payload.id), payload).then(() => {
+      router.push('/list')
+    })
+  }
+}
 
 const disabledSaveButton = computed<boolean>(() => {
   return !(company.value && vacancyLink.value && hrName.value)
@@ -37,7 +63,7 @@ const disabledSaveButton = computed<boolean>(() => {
         <app-input-text
           v-model="contactWhatsApp"
           class="input mb-3"
-          placeholder="WhatsApp phone Nik HR"
+          placeholder="WhatsApp phone HR"
         />
         <app-input-text v-model="contactPhone" class="input mb-3" placeholder="Phone HR" />
         <app-button
