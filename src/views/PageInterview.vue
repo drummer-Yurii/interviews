6c3fc499bf2fb0b21e1b4a52739dc3b970009ae7
Yurii Rybachok <drummer2009@ukr.net>
2024-05-15 17:52:56 +0300

    add data to template

--- src/views/PageInterview.vue
+++ src/views/PageInterview.vue
@@ -27,33 +27,33 @@ onMounted(async () => await getData())
 
 <template>
     <app-progress v-if="isLoading" />
-    <div class="content-interview" v-else>
+    <div class="content-interview" v-else-if="interview?.id && !isLoading">
         <app-card>
-            <template #title>Interview at the company</template>
+            <template #title>Interview at the company {{ interview.company}}</template>
             <template #content>
                 <div class="flex flex-column gap-2">
                     <label for="company">Company</label>
-                    <app-input-text class="input mb-3" id="company" />
+                    <app-input-text class="input mb-3" id="company" v-model="interview.company" />
                 </div>
                 <div class="flex flex-column gap-2">
                     <label for="vacancyLink">Vacancy description (link)</label>
-                    <app-input-text class="input mb-3" id="vacancyLink" />
+                    <app-input-text class="input mb-3" id="vacancyLink" v-model="interview.vacancyLink" />
                 </div>
                 <div class="flex flex-column gap-2">
                     <label for="hrName">Contact (name)</label>
-                    <app-input-text class="input mb-3" id="hrName" />
+                    <app-input-text class="input mb-3" id="hrName" v-model="interview.hrName" />
                 </div>
                 <div class="flex flex-column gap-2">
                     <label for="contactTelegram">Telegram username HR</label>
-                    <app-input-text class="input mb-3" id="contactTelegram" />
+                    <app-input-text class="input mb-3" id="contactTelegram" v-model="interview.contactTelegram" />
                 </div>
                 <div class="flex flex-column gap-2">
                     <label for="contactWhatsApp">WhatsApp HR</label>
-                    <app-input-text class="input mb-3" id="contactWhatsApp" />
+                    <app-input-text class="input mb-3" id="contactWhatsApp" v-model="interview.contactWhatsApp" />
                 </div>
                 <div class="flex flex-column gap-2">
                     <label for="contactPhone">Phone HR</label>
-                    <app-input-text class="input mb-3" id="contactPhone" />
+                    <app-input-text class="input mb-3" id="contactPhone" v-model="interview.contactPhone" />
                 </div>
                 <div class="flex flex-wrap gap-3 p-fluid mb-3">
                     <div class="flex-auto">
