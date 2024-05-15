7e12b8266eab7fd76f300c9681c84ba559f2bc37
Yurii Rybachok <drummer2009@ukr.net>
2024-05-15 15:23:59 +0300

    create form for page interview

--- src/views/PageInterview.vue
+++ src/views/PageInterview.vue
@@ -3,9 +3,53 @@
 </script>
 
 <template>
-    <h1>Interview</h1>
+    <div class="content-interview">
+        <app-card>
+            <template #title>Interview at the company</template>
+            <template #content>
+                <div class="flex flex-column gap-2">
+                    <label for="company">Company</label>
+                    <app-input-text class="input mb-3" id="company" />
+                </div>
+                <div class="flex flex-column gap-2">
+                    <label for="vacancyLink">Vacancy description (link)</label>
+                    <app-input-text class="input mb-3" id="vacancyLink" />
+                </div>
+                <div class="flex flex-column gap-2">
+                    <label for="hrName">Contact (name)</label>
+                    <app-input-text class="input mb-3" id="hrName" />
+                </div>
+                <div class="flex flex-column gap-2">
+                    <label for="contactTelegram">Telegram username HR</label>
+                    <app-input-text class="input mb-3" id="contactTelegram" />
+                </div>
+                <div class="flex flex-column gap-2">
+                    <label for="contactWhatsApp">WhatsApp HR</label>
+                    <app-input-text class="input mb-3" id="contactWhatsApp" />
+                </div>
+                <div class="flex flex-column gap-2">
+                    <label for="contactPhone">Phone HR</label>
+                    <app-input-text class="input mb-3" id="contactPhone" />
+                </div>
+            </template>
+        </app-card>
+    </div>
 </template>
 
 <style scoped>
+.content-interview {
+    max-width: 600px;
+    margin: auto;
+}
 
+.input {
+    width: 100%;
+}
+
+.interview-stage {
+    border: 1px solid #ccc;
+    border-radius: 6px;
+    padding: 10px;
+    margin-bottom: 10px;
+}
 </style>
